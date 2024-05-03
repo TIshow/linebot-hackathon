ab73390d61383614e068b1159cf85953c0f85ac1
TIshow <febooooooo@gmail.com>
2024-05-04 01:35:02 +0900

    wip

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  liff.init({ liffId:  "2004698497-4e8mJBR3" }).then(() => {
+  liff.init({ liffId:  "liffId" }).then(() => {
     if (liff.isLoggedIn()) {
       liff.getProfile().then(profile => {
         const userId = profile.userId;
